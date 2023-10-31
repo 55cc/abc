6f386746c01f2592737843f55ce1010636045828
55cc <12018747+55cc@users.noreply.github.com>
2023-10-31 16:20:43 +0800

    Update zun.sh

--- zun.sh
+++ zun.sh
@@ -7,6 +7,12 @@ echo -e "# abc\n" > ./README.md
 echo  $msg >> ./README.md
 echo -e "\n" >> ./README.md
 
+
+echo "$GIT_NAME $GIT_EMAIL $USER"
+
+
+git config user.email "$GIT_EMAIL"
+git config user.name "$GIT_NAME"
 git add .
 git commit -m "$msg";
-git push
+git push https://$USER:$PSWD@github.com/$USER/abc
